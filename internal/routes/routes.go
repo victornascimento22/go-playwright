c0dff083c90cbe5355717cdf9e305a92efcd8c4d
Victor <equipesi@capitaltrade.srv.br>
2024-12-18 15:24:57 -0300

    changes

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -1,5 +1,9 @@
-package internal
+package routes
 
+import (
+"gitlab.com/applications2285147/api-go/internal"
+
+)
 func RegisterRoutes(router *gin.Engine){
 
 	router.GET("/health", func(c *gin.Context){
